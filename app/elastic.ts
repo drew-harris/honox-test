ca617722836f7398af98881afbd8dbc471b9a730
Drew Harris <harrisd@smu.edu>
2024-04-07 11:20:18 -0500

    Remove try/catch from elastic

--- app/elastic.ts
+++ app/elastic.ts
@@ -1,21 +1,13 @@
 import * as elasticsearch from "@elastic/elasticsearch";
 import { env } from "./env";
 
-var elastic: elasticsearch.Client;
-
-try {
-  elastic = new elasticsearch.Client({
-    node: env.ELASTIC_NODE,
-    auth: {
-      username: "elastic",
-      password: env.ELASTIC_PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-} catch (err) {
-  console.error(err);
-}
-
-export { elastic };
+export const elastic = new elasticsearch.Client({
+  node: env.ELASTIC_NODE,
+  auth: {
+    username: "elastic",
+    password: env.ELASTIC_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
